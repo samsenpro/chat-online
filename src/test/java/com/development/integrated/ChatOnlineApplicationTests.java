bcbaffd04108a9233ae29e5fa12042187c8102be
samsenpro <samueldavidmartinez0@gmail.com>
2025-02-18 17:42:23 -0500

    creacion del proyecto

---
+++ src/test/java/com/development/integrated/ChatOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.integrated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
